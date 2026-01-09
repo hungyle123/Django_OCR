9de4cb17c81e4dbf343641d8a95793f275337217
hungyle123 <hungyle123@gmail.com>
2026-01-09 18:32:58 +0700

    feat(core): Test model Paddle OCR (good)

--- core/views.py
+++ core/views.py
@@ -33,18 +33,28 @@ def upload_file(request):
                 result = ocr_model.ocr(img_path)
 
                 raw_text_lines = []
-                # Kiểm tra result[0] có dữ liệu không (tránh lỗi nếu ảnh trắng trơn)
-                print("\n" + "="*50)
-                print("--- CẤU TRÚC DỮ LIỆU PADDLE TRẢ VỀ ---")
-                pprint.pprint(result)  # In đẹp (Pretty Print)
-                print("="*50 + "\n")
-                # if result and result[0]:
-                #     for i,line in enumerate(result[0]):
-                #         # Cấu trúc line: [ [tọa độ], ("nội dung chữ", 0.99) ]
-                #         text_content = line[1][0] 
-                #         raw_text_lines.append(text_content)
-                #         print(i)
-                #         print(line)
+
+                # ['input_path', 'page_index', 'doc_preprocessor_res', 'dt_polys', 'model_settings', 'text_det_params', 'text_type', 'text_rec_score_thresh', 'return_word_box', 'rec_texts', 'rec_scores', 'rec_polys', 'vis_fonts', 'textline_orientation_angles', 'rec_boxes']
+                print(result[0]['rec_texts'])
+                if(result[0]['rec_texts'] is None):
+                    print("None")
+                print(len(result[0]['rec_texts']))
+
+                # print(result[0]['vis_fonts'])
+                # if(result[0]['vis_fonts'] is None):
+                #     print("None")
+                # print("\n" + "="*50)
+                # print("--- CẤU TRÚC DỮ LIỆU PADDLE TRẢ VỀ ---")
+                # pprint.pprint(result)  # In đẹp (Pretty Print)
+                # print("="*50 + "\n")
+                # # Kiểm tra result[0] có dữ liệu không (tránh lỗi nếu ảnh trắng trơn)
+                # # if result and result[0]:
+                # #     for i,line in enumerate(result[0]):
+                # #         # Cấu trúc line: [ [tọa độ], ("nội dung chữ", 0.99) ]
+                # #         text_content = line[1][0] 
+                # #         raw_text_lines.append(text_content)
+                # #         print(i)
+                # #         print(line)
                 
                 text_result = "\n".join(raw_text_lines)
 
