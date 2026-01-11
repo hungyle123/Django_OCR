f9a2abff04015b620be73a11997f25ba0247f2b4
hungyle123 <hungyle123@gmail.com>
2026-01-11 23:24:29 +0700

    Comment the import

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import DocumentForm
 from .models import Document, Invoices, InvoiceItem
-import pytesseract
+# import pytesseract
 # from PIL import Image
 # from django.conf import settings
 import os
